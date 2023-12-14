8250fdd593cbb103bd0531e657162125ed5c10d6
Patrik <144269021+gameovercheeseburger@users.noreply.github.com>
2023-12-13 21:46:01 -0700

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,17 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+document.addEventListener('DOMContentLoaded', function () {
+    const submitButton = document.getElementById('submit-button');
+
+    if (submitButton) {
+        submitButton.addEventListener('click', function () {
+            const contactPage = document.getElementById('contact-page');
+
+            if (contactPage) {
+                // Replace the content of the contact page with a thank you message
+                contactPage.innerHTML = '<p style="font-size: 24px;">Thank you for your message</p>';
+            }
+        });
+    }
+});
